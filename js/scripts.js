88eaf3daa9021eef05d32a12597d406f42585b5d
Kate Farkas <kate.m.farkas@gmail.com>
2020-03-26 09:11:44 -0700

    link jq correctly like a non-noob; add else if statement

--- js/scripts.js
+++ js/scripts.js
@@ -2,6 +2,8 @@ $(document).ready(function() {
   var age = parseInt(prompt("How old are you?"));
   if (age >= 21) {
     $("#drinks").show();
+  } else if (age === 21) {
+    alert("Happy birthyear! Go crazy!")
   } else {
     $("#under-21").show();
   }
