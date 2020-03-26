475ba98af6a45e809b6260bc6109eafc89781a50
Kate Farkas <kate.m.farkas@gmail.com>
2020-03-26 09:19:37 -0700

    remove errant >= causing elif statement to not run :/

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,23 @@
 $(document).ready(function() {
   var age = parseInt(prompt("How old are you?"));
-  if (age >= 21) {
+ 
+  if (age > 21) {
     $("#drinks").show();
   } else if (age === 21) {
-    alert("Happy birthyear! Go crazy!")
+    alert("Happy birthyear! Go crazy!");
+    $("#drinks").show();
   } else {
     $("#under-21").show();
   }
 });
+
+// simpler yes/no version
+// $(document).ready(function() {
+//   var over21 = confirm("Are you over 21? Click OK for yes or Cancel for no.");
+
+//   if (over21) {
+//     $('#drinks').show();
+//   } else {
+//     $('#under-21').show();
+//   }
+// });
